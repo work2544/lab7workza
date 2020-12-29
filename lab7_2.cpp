5b33fca6991d57090651366b3a4a63f1442ecf69
work2544 <work21_123@hotmail.com>
2020-12-29 16:39:34 +0700

    complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,36 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 #include<iomanip>
-#include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
 
+
+char findGrade(float score)
+{
+return score>90?'A' :score>75?'B' :score>60?'C':score>45?'D':'F';
+}
 int main(){
 	//Input the number of students
-	int N,i = 0;
+	int N,j = 0;
+	
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
-	float score[N];	
-	
+	float score[N];
 	//Store names and scores of students into an array 
-	while(i < N){
+	    for(int i=0;i<N;i++){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
+		getline(cin,name[i]);
+		//cin>>name[i];
+		//cin.ignore();
+		
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
+		cin>>score[i]; 	
 		//[Missing Code 3] Get score of the i-th students.
-		i++;
 	}
 	
 	//Print names scores and grades
-	i = 0;
+	j = 0;
 	cout << "---------------------------------------------\n";
 	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
 	cout << "---------------------------------------------\n";
-	while(i < N){
-		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
-		i++;
+	while(j < N){
+		cout << setw(25) << name[j] << setw(8) << score[j] << setw(8) << findGrade(score[j]) << "\n";
+		j++;
 	} 
 	cout << "---------------------------------------------\n";
 
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,59 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+#include<cmath>
+using namespace  std;
+int main()
+{
+   int age,height,property;
+   string status;
+cout<<"Enter your age: ";
+cin>>age;
+//1
+if(age<=20)
+{
+cout<<"Enter your height: ";
+cin>>height;
+if(height<160)
+status="UNFRIEND";
+else if(height<175)
+{
+    status="FRIEND";
+}
+else
+{
+cout<<"Enter your property: ";
+cin>>property;
+if(property>69*pow(10,6))
+{
+    status="MARRIED";
+}
+else
+{
+    status="ONE-NIGHT-STAND";
+}
+
+}
+}
+//2
+else if(age<30)
+{
+cout<<"Enter your property: ";
+cin>>property;
+if(property>pow(10,7))
+{
+    status="BEST FRIEND";
+}
+else
+{
+    status="UNFRIEND";
+}
+}
+//3
+else 
+{
+    status="UNFRIEND";
+}
+cout<<"Your status = "<<status;
+}
+   
+
+
